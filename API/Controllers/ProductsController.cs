d1b9f06509127531d6a12b8265206057411dc7db
Pedro <phts99@gmail.com>
2022-01-13 22:18:16 -0300

    Implemented search by product id specification on  get request task

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -34,7 +34,8 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _productsRepo.GetByIdAsync(id);
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            return await _productsRepo.GetEntityWithSpec(spec);
         }
 
         [HttpGet("brands")]
