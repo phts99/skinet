d3a1bde67fc949ba81dd72e1cf61e93c81c7f106
Pedro <phts99@gmail.com>
2022-01-15 19:56:52 -0300

    Implemented api response to 404 not found and 400 bad request

--- API/Controllers/BugController.cs
+++ API/Controllers/BugController.cs
@@ -1,3 +1,4 @@
+using API.Errors;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,7 +19,7 @@ namespace API.Controllers
 
             if (thing == null)
             {
-                return NotFound();
+                return NotFound(new ApiResponse(404));
             }
 
             return Ok();
@@ -37,7 +38,7 @@ namespace API.Controllers
         [HttpGet("badrequest")]
         public ActionResult GetBadRequest()
         {
-            return BadRequest();
+            return BadRequest(new ApiResponse(400));
         }
 
         [HttpGet("badrequest/{id}")]
