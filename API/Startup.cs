86dc12bc5dd48e7c860aabcc54266bfd9c911215
Pedro <phts99@gmail.com>
2022-01-16 17:19:55 -0300

    Added swagger for all environments

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,11 +56,8 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>();
 
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
