3a95464e3bdf7a787056236a076adb2dc8443bb0
Pedro <phts99@gmail.com>
2022-01-17 22:32:05 -0300

    Added pagination helper class

---
+++ API/Helpers/Pagination.cs
@@ -0,0 +1,10 @@
+namespace API.Helpers
+{
+    public class Pagination<T> where T : class
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int Count { get; set; }
+        public IReadOnlyList<T> Data { get; set; }
+    }
+}
