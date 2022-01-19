189e7591aa641d1c51aeedd23b6dab38f7f0bb5b
Pedro <phts99@gmail.com>
2022-01-18 22:11:39 -0300

    Added CORS policy for angular localhost

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,6 +25,13 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +46,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
