d31ba75600007eb33bcf113429103f680d0137ac
phts99 <phts99@gmail.com>
2022-07-05 22:13:42 -0300

    ci(project architecture): change Models component to shared module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/products';
-import { IPagination } from './models/pagination';
+import { IProduct } from './shared/models/products';
+import { IPagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
