921695f0ce09bc0b71a77c4002749d693fed74de
phts99 <phts99@gmail.com>
2022-07-05 22:04:45 -0300

    feat(project architecture): add core module

---
+++ client/src/app/core/core.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    NavBarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NavBarComponent,
+  ]
+})
+
+export class CoreModule { }
