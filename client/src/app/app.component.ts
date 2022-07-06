f8f531a33b94b63f6ac59fd9b388c0d22b1efda5
phts99 <phts99@gmail.com>
2022-07-05 21:23:20 -0300

    feat(main page): retrieve product data from API

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IProduct } from './models/products';
+import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,13 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Skinet';
-  products: any[];
+  products: IProduct[];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: any) => {
+      (response: IPagination) => {
         this.products = response.data;
       },
       (error) => {
