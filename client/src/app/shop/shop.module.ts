c9b4e4242f1d401cdd7a0ccf92e720e305f3e15c
phts99 <phts99@gmail.com>
2022-07-07 21:46:11 -0300

    feat(product display): add static product child component

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule
